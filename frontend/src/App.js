11000c35f068b67a0591b7c8aca3b3f07fbfc45a
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-01-01 17:55:24 -0500

    updated styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
-      Hello, from App.js
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Sellers Market</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 

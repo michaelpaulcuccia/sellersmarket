5d803f6bdab897289adfb8ee83496ed9b71b7602
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-01-04 13:34:49 -0500

    reactrouterdom, reactrouterbootstrap linkage

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -6,13 +7,18 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Sellers Market</Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand >Sellers Market</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Link href="/basket"><i className="fas fa-shopping-basket"></i> basket</Nav.Link>
-                            <Nav.Link href="/login"><i className="fas fa-user-circle"></i> login</Nav.Link>
-
+                            <LinkContainer to="/basket">
+                                <Nav.Link><i className="fas fa-shopping-basket"></i> basket</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link><i className="fas fa-user-circle"></i> login</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>

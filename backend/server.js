f791f13ae40c6e13f1abe7f7cb2bcd339af8026c
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-01-05 17:10:17 -0500

    DB connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import colors from 'colors';
 import products from '../backend/data/products.js';
 
 //initialize dotenv
@@ -7,6 +9,25 @@ dotenv.config()
 
 //initialize express
 const app = express();
+
+//Database Connection with Mongoose
+(async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`.blue);
+
+    } catch (err) {
+        console.error(`Error: ${err.message}`.red.bold);
+        //exit process with failure
+        process.exit(1);
+    }
+})();
 
 //TEST
 app.get('/', (req, res) => {
@@ -26,4 +47,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => { console.log(`App listening in ${process.env.NODE_ENV} mode on port ${PORT}`) });
+app.listen(PORT, () => { console.log(`App listening in ${process.env.NODE_ENV} mode on port ${PORT}`.brightGreen) });

ca8d8f79975999438382db44e3e9ccbeac880e51
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-01-25 15:13:43 -0500

    payment screen and save payment method

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,7 +1,8 @@
 import {
     CART_ADD_ITEM,
     CART_REMOVE_ITEM,
-    CART_SAVE_SHIPPING_ADDRESS
+    CART_SAVE_SHIPPING_ADDRESS,
+    CART_SAVE_PAYMENT_METHOD
 } from '../constants/cartConstants';
 import axios from 'axios';
 
@@ -47,4 +48,13 @@ export const saveShippingAddress = (data) => (dispatch) => {
     })
 
     localStorage.setItem('shippingAddress', JSON.stringify(data))
+}
+
+export const savePaymentMethod = (data) => (dispatch) => {
+    dispatch({
+        type: CART_SAVE_PAYMENT_METHOD,
+        payload: data
+    })
+
+    localStorage.setItem('paymentMethod', JSON.stringify(data))
 }

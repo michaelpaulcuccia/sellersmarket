74a1f8e5ea1973e1a8a49abd5330ba35fc443777
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-01-25 11:19:05 -0500

    checkout steps component

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -4,7 +4,6 @@ import { Form, Button, Row, Col, Image } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-//import FormContainer from '../components/FormContainer';
 import { register } from '../actions/userActions';
 import registerImage from '../appImages/registerImage.png';
 
@@ -46,66 +45,6 @@ const RegisterScreen = ({ location, history }) => {
     };
 
     return (
-        // <FormContainer>
-        //     <h1>Sign Up</h1>
-
-        //     {message && <Message variant='danger'>{message}</Message>}
-        //     {error && <Message variant='danger'>{error}</Message>}
-        //     {loading && <Loader />}
-
-        //     <Form onSubmit={submitHandler}>
-
-        //         <Form.Group controlId='name'>
-        //             <Form.Label>Name</Form.Label>
-        //             <Form.Control
-        //                 type="name"
-        //                 placeholder='Enter Name'
-        //                 value={name}
-        //                 onChange={(event) => setName(event.target.value)}>
-        //             </Form.Control>
-        //         </Form.Group>
-
-        //         <Form.Group controlId='email'>
-        //             <Form.Label>Email Address</Form.Label>
-        //             <Form.Control
-        //                 type="email"
-        //                 placeholder='Enter Email'
-        //                 value={email}
-        //                 onChange={(event) => setEmail(event.target.value)}>
-        //             </Form.Control>
-        //         </Form.Group>
-
-        //         <Form.Group controlId='password'>
-        //             <Form.Label>Password</Form.Label>
-        //             <Form.Control
-        //                 type="password"
-        //                 placeholder='Enter Password'
-        //                 value={password}
-        //                 onChange={(event) => setPassword(event.target.value)}>
-        //             </Form.Control>
-        //         </Form.Group>
-
-        //         <Form.Group controlId='confirmPassword'>
-        //             <Form.Label>Confirm Password</Form.Label>
-        //             <Form.Control
-        //                 type="password"
-        //                 placeholder='Confirm Password'
-        //                 value={confirmPassword}
-        //                 onChange={(event) => setConfirmPassword(event.target.value)}>
-        //             </Form.Control>
-        //         </Form.Group>
-
-        //         <Button type='submit' variant='primary'>Register</Button>
-
-        //     </Form>
-
-        //     <Row className='py-3'>
-        //         <Col>
-        //             Already have an account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link>
-        //         </Col>
-        //     </Row>
-
-        // </FormContainer>
 
         <Row>
 

e5b205f7417c3d7a627823e3c2f80e7b53547e16
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-02-16 09:56:00 -0500

    updated server

--- backend/server.js
+++ backend/server.js
@@ -52,9 +52,13 @@ app.use(errorHandler);
 
 //deployment
 const __dirname = path.resolve();
+
+//serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
 }
 
 const PORT = process.env.PORT || 5000;

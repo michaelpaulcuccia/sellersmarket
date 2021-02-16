dce788c5b003b9c602f3fe0503a261938a3a86a8
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-02-16 09:24:01 -0500

    update server, define dirname

--- backend/server.js
+++ backend/server.js
@@ -51,6 +51,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 //deployment
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')));
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
